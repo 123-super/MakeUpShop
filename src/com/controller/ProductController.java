c8ddb4fbcaeb648010c499b23be082c9193b36cd
HeMOu <fangqichenchao@163.com>
2021-05-17 23:08:57 +0800

    图片详情显示

--- src/com/controller/ProductController.java
+++ src/com/controller/ProductController.java
@@ -1,17 +1,22 @@
 package com.controller;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.bean.Category;
 import com.bean.Product;
-import com.bean.PropertyValue;
 import com.common.Result;
 import com.service.ProductService;
 
@@ -37,4 +42,22 @@ public class ProductController {
 		
 	}
 	
+	@RequestMapping(value = "/productImage/{pid}", method = RequestMethod.GET, produces = "application/json;charsest=utf-8")
+	@ResponseBody
+	public String productImage(@PathVariable String pid, HttpServletRequest request) {
+		String rootPath = "/images/productDetail/" + pid;
+		String realPath = request.getServletContext().getRealPath(rootPath);
+		File file = new File(realPath);
+		if (!file.exists()) {
+			return JSON.toJSONString(Result.error());
+		} else {
+			String[] pathList = file.list();
+			if (pathList.length > 0) {
+				List<String> imgPathList = Arrays.asList(pathList).stream().map(item -> rootPath + "/" + item).collect(Collectors.toList());
+				return JSON.toJSONString(Result.success(imgPathList));
+			} else {
+				return JSON.toJSONString(Result.error());
+			}
+		}
+	}
 }
